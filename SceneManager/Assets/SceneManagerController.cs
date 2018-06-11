3b9fe31875fb867a76c1692672af87e476b9c496
moyoya <hubuki008@gmail.com>
2018-06-11 11:16:57 +0900

    [準備]

---
+++ SceneManager/Assets/SceneManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //現在のシーンの名前を取得する
+        //Debug.Log(SceneManager.GetActiveScene().name);
+
+        Debug.Log(SceneManager.sceneCount);
+
+        for(int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
