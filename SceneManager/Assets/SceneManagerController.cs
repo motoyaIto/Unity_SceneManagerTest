efa20cf2985d76b06b605ac3c593ea642e5b9297
moyoya <hubuki008@gmail.com>
2018-06-11 11:57:39 +0900

    てす

--- SceneManager/Assets/SceneManagerController.cs
+++ SceneManager/Assets/SceneManagerController.cs
@@ -10,12 +10,19 @@ public class SceneManagerController : MonoBehaviour {
         //現在のシーンの名前を取得する
         //Debug.Log(SceneManager.GetActiveScene().name);
 
+        //シーンをロードする
+        SceneManager.LoadScene("Scene2");
+
+        //読み込まれてるシーン数を取得する
         Debug.Log(SceneManager.sceneCount);
 
+        //読み込まれてるシーンの名前を取得
         for(int i = 0; i < SceneManager.sceneCount; i++)
         {
-            
+            Debug.Log(SceneManager.GetSceneAt(i).name);
         }
+
+        
     }
 	
 	// Update is called once per frame
